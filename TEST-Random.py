7cad1ccd153b49c4daf9b9404252f8e37dd4aeb0
ajlongst <ajlongst@gmail.com>
2017-03-08 11:54:44 -0500

    Update and rename status.py to TEST-Random.py

--- TEST-Random.py
+++ TEST-Random.py
@@ -9,11 +9,6 @@ unicorn.set_layout(unicorn.HAT)
 unicorn.rotation(0)
 unicorn.brightness(.4)
 
-
-#unicorn.set_pixel(x,y,0,150,0)
-#unicorn.show()
-#time.sleep(5)
-
 while True:
     unicorn.set_pixel(randint(0,7),randint(0,7),randint(100,150),randint(100,200),randint(150,200))
     unicorn.show()
